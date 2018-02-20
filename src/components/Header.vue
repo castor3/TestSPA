32e4ec138f4dd68d83a639e62f624834e14cf174
Rui Santos <rui@Ruis-MBP.home>
2018-02-20 00:06:17 +0000

    Added login UI

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,33 +1,44 @@
 <template>
 	<div style="width:100%">
+		<!-- Header -->
 		<el-header style="font-size: 12px">
-			<el-row>
-				<span style="float: left">
-					<el-col>
-						<!-- Collapse / Expand buttons -->
-						<el-button @click="toggleIsCollapsed" size="mini" round>Menu</el-button>
-					</el-col>
-				</span>
-				<span style="float: right">
-					<el-col>
-						<!-- Right side options -->
-						<el-dropdown>
-							<i class="el-icon-setting" style="margin-right: 15px"></i>
-							<el-dropdown-menu slot="dropdown">
-								<el-dropdown-item>View</el-dropdown-item>
-								<el-dropdown-item>Add</el-dropdown-item>
-								<el-dropdown-item>Delete</el-dropdown-item>
-							</el-dropdown-menu>
-						</el-dropdown>
-						<span>Rui</span>
-					</el-col>
-				</span>
-			</el-row>
+			<span style="float: left">
+				<el-col>
+					<el-button @click="toggleIsCollapsed" size="mini" round>Menu</el-button>
+				</el-col>
+			</span>
+			<span style="float: right">
+				<el-col>
+					<!-- Right side options -->
+					<el-dropdown>
+						<i class="el-icon-setting" style="margin-right: 15px"></i>
+						<el-dropdown-menu slot="dropdown">
+							<el-dropdown-item>View</el-dropdown-item>
+							<el-dropdown-item>Add</el-dropdown-item>
+							<el-dropdown-item>Delete</el-dropdown-item>
+						</el-dropdown-menu>
+					</el-dropdown>
+					<span>Rui</span>
+				</el-col>
+			</span>
 		</el-header>
 
 		<!-- Main content -->
 		<el-main>
-			<h1>Main content</h1>
+			<div id="centerLogin">
+				<h1>Login</h1>
+				<el-form>
+					<el-form-item>
+						<el-input placeholder="username" v-model="username"></el-input>
+					</el-form-item>
+					<el-form-item>
+						<el-input type="password" placeholder="password"></el-input>
+					</el-form-item>
+					<el-form-item>
+						<el-button @click="onSubmit" type="primary">Login</el-button>
+					</el-form-item>
+				</el-form>
+			</div>
 		</el-main>
 	</div>
 </template>
@@ -44,24 +55,27 @@ export default {
     "el-dropdown": element.Dropdown,
     "el-dropdown-menu": element.DropdownMenu,
     "el-dropdown-item": element.DropdownItem,
-    "el-radio-group": element.RadioGroup,
-    "el-radio-button": element.RadioButton,
-    "el-row": element.Row,
     "el-col": element.Col,
+
+    // Main
     "el-main": element.Main,
+    "el-form": element.Form,
+    "el-form-item": element.FormItem,
+    "el-input": element.Input,
+
     "el-button": element.Button
   },
   data() {
     return {
       isCollapsed: true,
-      testProperty: ""
+      testProperty: "",
+      username: ""
     };
   },
   props: ["collapseValue"],
   methods: {
-    buttonClicked() {
-      alert("mensagem");
-      this.testProperty = "newValue";
+    onSubmit() {
+      console.log("submit!");
     },
     toggleIsCollapsed() {
       this.isCollapsed = !this.isCollapsed;
@@ -73,14 +87,15 @@ export default {
 </script>
 
 <style>
+#centerLogin {
+  width: 25%;
+  margin: 0 auto;
+}
 .el-header {
   background-color: #b3c0d1;
   color: #333;
   text-align: center;
   line-height: 60px;
-}
-.el-row {
-  margin-bottom: 20px;
 }
 .el-col {
   border-radius: 4px;
